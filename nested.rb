2e522ca0f28fec1d5797e4e3783318c17101c064
Amir Safoev <amirsafoev0@gmail.com>
2018-09-27 18:21:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -92,7 +92,7 @@ def changing_alan
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "GUI",
+          :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
@@ -106,8 +106,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
-     
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -126,5 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
+return programmer_hash
 end
